f7e2f8fb0917bf7a9df8980e108fa61eb93907de
goni715 <halesgoni715149123@gmail.com>
2025-06-08 20:23:35 +0600

    forgot & reset password form

--- src/components/auth/ForgotPasswordForm.tsx
+++ src/components/auth/ForgotPasswordForm.tsx
@@ -1,32 +1,35 @@
 "use client";
 
-import { MdEmail } from "react-icons/md";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import CustomInput from "../ui/CustomInput";
+import { z } from "zod";
+import { forgotPasswordSendOtpSchema } from "@/schemas/auth.schema";
+
+type TFormValues = z.infer<typeof forgotPasswordSendOtpSchema>;
 
 const ForgotPasswordForm = () => {
+  const { handleSubmit, control } = useForm({
+    resolver: zodResolver(forgotPasswordSendOtpSchema),
+  });
+
+  const onSubmit: SubmitHandler<TFormValues> = (data) => {
+    console.log(data);
+  };
   return (
     <>
-      <form className="space-y-4">
-        <div>
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <div className="relative mt-1">
-            <span className="absolute inset-y-0 left-3 flex items-center text-gray-400 text-lg">
-              <MdEmail />
-            </span>
-            <input
-              type="email"
-              id="email"
-              className="block w-full border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 focus:ring-blue-500 pl-10 pr-4 py-2"
-              placeholder="enter your email here"
-            />
-          </div>
-        </div>
-
-        <button className="w-full bg-primary cursor-pointer text-white py-2 rounded-md font-semibold transition-colors duration-100">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <CustomInput
+          label="Email"
+          name="email"
+          type="text"
+          control={control}
+          placeholder="Enter email address"
+        />
+        <button
+          type="submit"
+          className="w-full bg-primary cursor-pointer text-white py-2 rounded-md font-semibold transition-colors duration-100"
+        >
           Continue
         </button>
       </form>
--- src/components/auth/ResetPasswordForm.tsx
+++ src/components/auth/ResetPasswordForm.tsx
@@ -1,62 +1,44 @@
 "use client";
 
-import { useState } from "react";
-import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { z } from "zod";
+import CustomInput from "../ui/CustomInput";
+import { resetPasswordSchema } from "@/schemas/auth.schema";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+type TFormValues = z.infer<typeof resetPasswordSchema>;
 
 const ResetPasswordForm = () => {
-  const [showPassword, setShowPassword] = useState<boolean>(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
+  const { handleSubmit, control } = useForm({
+    resolver: zodResolver(resetPasswordSchema),
+  });
 
+  const onSubmit: SubmitHandler<TFormValues> = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
-      <form className="space-y-4">
-        <div>
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-700"
-          >
-           New Password
-          </label>
-          <div className="relative">
-            <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              className="mt-1 block w-full border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 focus:ring-blue-500 px-4 py-2 pr-10"
-              placeholder="********"
-            />
-            <span
-              className="absolute inset-y-0 right-3 flex items-center text-xl text-gray-500 cursor-pointer"
-              onClick={()=> setShowPassword((prev) => !prev)}
-            >
-              {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
-            </span>
-          </div>
-        </div>
-        <div>
-          <label
-            htmlFor="confirmPass"
-            className="block text-sm font-medium text-gray-700"
-          >
-           Confirm New Password
-          </label>
-          <div className="relative">
-            <input
-              type={showConfirmPassword ? "text" : "password"}
-              id="confirmPass"
-              className="mt-1 block w-full border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 focus:ring-blue-500 px-4 py-2 pr-10"
-              placeholder="********"
-            />
-            <span
-              className="absolute inset-y-0 right-3 flex items-center text-xl text-gray-500 cursor-pointer"
-              onClick={()=> setShowConfirmPassword((prev) => !prev)}
-            >
-              {showConfirmPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
-            </span>
-          </div>
-        </div>
-        <button className="w-full bg-primary hover:bg-[#2b4773] cursor-pointer text-white py-2 rounded-md font-semibold transition-colors duration-100">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <CustomInput
+          label="New Password"
+          name="newPassword"
+          type="password"
+          control={control}
+          placeholder="Enter new password"
+        />
+        <CustomInput
+          label="Confirm New Password"
+          name="confirmPassword"
+          type="password"
+          control={control}
+          placeholder="Enter new password"
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-primary hover:bg-[#2b4773] cursor-pointer text-white py-2 rounded-md font-semibold transition-colors duration-100"
+        >
           Set Password
         </button>
       </form>
