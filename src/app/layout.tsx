213064353cb85a8ef43e7ed7d7a1a8207b50c5a8
TayeburRahman <tayebrayhan101+@gmail.com>
2025-07-22 14:17:25 +0600

    updates all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,32 @@ export default function RootLayout({
   return (
     <GlobalProvider>
       <html lang="en">
+        <head>
+          {/* GTM Head Script */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id=GTM-XXXXXXX';f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-XXXXXXX');
+              `,
+            }}
+          />
+        </head>
         <body className={`${poppins.variable} antialiased`}>
+          {/* GTM NoScript */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+
           <NextTopLoader
-            //color="#2299DD"
             color="#EC4899"
             initialPosition={0.08}
             crawlSpeed={200}
@@ -35,7 +58,6 @@ export default function RootLayout({
             showSpinner={false}
             easing="ease"
             speed={200}
-            //shadow="0 0 10px #2299DD,0 0 5px #2299DD"
             shadow="0 0 10px #EC4899,0 0 5px #EC4899"
           />
           {children}
---
+++ src/components/GTM.tsx
@@ -0,0 +1,46 @@
+import Navbar from "@/components/shared/Navbar";
+import React from "react";
+
+export const metadata = {
+  title: "Your App Title",
+  description: "App description here",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        {/* GTM script (head) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-5VWGRHTN';f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5VWGRHTN');
+            `,
+          }}
+        />
+      </head>
+      <body>
+        {/* GTM noscript (body) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5VWGRHTN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
