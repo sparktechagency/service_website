6c4362eb465907372b10df8b1adf775df83cfb6e
TayeburRahman <tayebrayhan101+@gmail.com>
2025-07-22 14:44:40 +0600

    updates all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,28 +25,28 @@ export default function RootLayout({
     <GlobalProvider>
       <html lang="en">
         <head>
-          {/* GTM Head Script */}
+          {/* Google Tag Manager - Head Script */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id=GTM-XXXXXXX';f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXXXXX');
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5VWGRHTN');
               `,
             }}
           />
         </head>
         <body className={`${poppins.variable} antialiased`}>
-          {/* GTM NoScript */}
+          {/* Google Tag Manager - NoScript iframe */}
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5VWGRHTN"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
+            />
           </noscript>
 
           <NextTopLoader
